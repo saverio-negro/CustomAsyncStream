7672426a99a7b4f379dde3c92698412baa0549ed
saverio-negro <saverionegro@gmail.com>
2026-04-10 19:09:26 +0200

    Initial Commit

---
+++ CustomAsyncStream/CustomAsyncStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAsyncStreamApp.swift
+//  CustomAsyncStream
+//
+//  Created by Saverio Negro on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomAsyncStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
